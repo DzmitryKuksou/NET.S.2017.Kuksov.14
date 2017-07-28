3c02b679c331559daf19dee1016dfdf56f139bb5
dzm <vvv-rrrr@list.ru>
2017-07-28 23:05:36 +0300

    code

--- SetAndFibbonachy/Fibbonachy_s/Fibbonachy.cs
+++ SetAndFibbonachy/Fibbonachy_s/Fibbonachy.cs
@@ -8,6 +8,11 @@ namespace Fibbonachy_s
 {
     public static class Fibbonachy
     {
+        /// <summary>
+        /// sequence
+        /// </summary>
+        /// <param name="y">order</param>
+        /// <returns>array</returns>
         public static IEnumerable<int> FibbonachyNumbers(int y)
         {
             int first = 0;
