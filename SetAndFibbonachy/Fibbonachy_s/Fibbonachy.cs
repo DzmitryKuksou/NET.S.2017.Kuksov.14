c75ed6bc5f0fb8cc9fc04f417ac31be3464b6acc
dzm <vvv-rrrr@list.ru>
2017-07-28 23:03:13 +0300

    code

---
+++ SetAndFibbonachy/Fibbonachy_s/Fibbonachy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibbonachy_s
+{
+    public static class Fibbonachy
+    {
+        public static IEnumerable<int> FibbonachyNumbers(int y)
+        {
+            int first = 0;
+            int second = 1;
+            while (second < y || first < y)  
+            {
+                yield return first;
+                second += first;
+                yield return second;
+                first += second;
+            }
+
+        }
+    }
+}
---
+++ SetAndFibbonachy/NUnit.FibbonachyTests1/TestFibbonachy.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibbonachy_s;
+
+namespace NUnit.FibbonachyTests1
+{
+    [TestFixture]
+    public class TestFibbonachy
+    {
+        [TestCase(5, ExpectedResult = new int[] { 0, 1, 1, 2, 3, 5 })]
+        [TestCase(20, ExpectedResult = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+        public IEnumerable<int> Test_FibbonachyNumbers(int y)
+        {
+            return Fibbonachy.FibbonachyNumbers(y);
+        }
+    }
+}
